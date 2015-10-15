ac20af36db1730a089ddfbf1e3280154794d6c44
Samraat Pawar <s.pawar@imperial.ac.uk>
2015-10-15 13:58:25 +0100

    Updated Notes

--- Code/LV1.py
+++ Code/LV1.py
@@ -7,23 +7,22 @@ import pylab as p #Contains matplotlib for plotting
 
 # import matplotlip.pylab as p #Some people might need to do this
 
-def dx_dt(z, t=0):
+def dR_dt(pops, t=0):
     """ Returns the growth rate of predator and prey populations at any 
     given time step """
     
-    x = z[0]
-    y = z[1]
+    R = pops[0]
+    C = pops[1]
+    dRdt = r*R - a*R*C 
+    dydt = -z*C + e*a*R*C
     
-    dxdt = r*x - a*x*y 
-    dydt = -m*y + e*a*x*y
-    
-    return sc.array([dxdt, dydt])
+    return sc.array([dRdt, dydt])
 
 # Define parameters:
-r = 1. # Prey growth rate
-a = 0.1 # Predator search rate (determines consumption rate) 
-m = 1.5 # Predator mortality rate
-e = 0.75 # Predator production efficiency
+r = 1. # Resource growth rate
+a = 0.1 # Consumer search rate (determines consumption rate) 
+z = 1.5 # Consumer mortality rate
+e = 0.75 # Consumer production efficiency
 
 # Now define time -- integrate from 0 to 15, using 1000 points:
 t = sc.linspace(0, 15,  1000)
@@ -32,18 +31,18 @@ x0 = 10
 y0 = 5 
 z0 = sc.array([x0, y0]) # initials conditions: 10 prey and 5 predators per unit area
 
-z, infodict = integrate.odeint(dx_dt, z0, t, full_output=True)
+pops, infodict = integrate.odeint(dR_dt, z0, t, full_output=True)
 
-infodict['message']                     # >>> 'Integration successful.'
+infodict['message']     # >>> 'Integration successful.'
 
-prey, predators = z.T # What's this for?
+prey, predators = pops.T # What's this for?
 f1 = p.figure() #Open empty figure object
-p.plot(t, prey, 'g-', label='Prey density') # Plot
-p.plot(t, predators  , 'b-', label='Predator density')
+p.plot(t, prey, 'g-', label='Resource density') # Plot
+p.plot(t, predators  , 'b-', label='Consumer density')
 p.grid()
 p.legend(loc='best')
 p.xlabel('Time')
 p.ylabel('Population')
-p.title('Predator-prey population dynamics')
+p.title('Consumer-prey population dynamics')
 p.show()
 f1.savefig('prey_and_predators_1.pdf') #Save figure
