e1703eab1bf8cf4dc4c994d49c0d0af1d97b5bc5
Samraat Pawar <s.pawar@imperial.ac.uk>
2015-11-09 10:55:53 +0000

    Added week 6 Code

---
+++ Code/using_os.py
@@ -0,0 +1,30 @@
+""" This is blah blah"""
+# Use the subprocess.os module to get a list of files and directories 
+# Hint: look in subprocess.os and/or subprocess.os.path for helpful 
+# functions
+
+import subprocess
+
+help(subprocess.os.listdir)
+
+# try:
+
+subprocess.os.listdir('../../')
+
+#################################
+#~Get a list of files and 
+#~directories in your home/ that start with an uppercase 'C'
+
+# Type your code here:
+
+#################################
+# Get files and directories in your home/ that start with either an 
+# upper or lower case 'C'
+
+# Type your code here:
+
+#################################
+# Get only directories in your home/ that start with either an upper or 
+#~lower case 'C' 
+
+# Type your code here:
