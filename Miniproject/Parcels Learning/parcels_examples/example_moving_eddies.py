6eba4663dccbfa7df84dfc64b569c386729f5d69
Alexander Kier Christensen <akc17@ic.ac.uk>
2018-01-16 11:50:45 +0000

    Added Parcels folder.

---
+++ Miniproject/Parcels Learning/parcels_examples/example_moving_eddies.py
@@ -0,0 +1,186 @@
+from parcels import FieldSet, ParticleSet, ScipyParticle, JITParticle
+from parcels import AdvectionRK4, AdvectionEE, AdvectionRK45
+from argparse import ArgumentParser
+import numpy as np
+import math
+import pytest
+from datetime import timedelta as delta
+from os import path
+
+
+ptype = {'scipy': ScipyParticle, 'jit': JITParticle}
+method = {'RK4': AdvectionRK4, 'EE': AdvectionEE, 'RK45': AdvectionRK45}
+
+
+def moving_eddies_fieldset(xdim=200, ydim=350):
+    """Generate a fieldset encapsulating the flow field consisting of two
+    moving eddies, one moving westward and the other moving northwestward.
+
+    Note that this is not a proper geophysical flow. Rather, a Gaussian eddy is moved
+    artificially with uniform velocities. Velocities are calculated from geostrophy.
+    """
+    # Set NEMO fieldset variables
+    depth = np.zeros(1, dtype=np.float32)
+    time = np.arange(0., 8. * 86400., 86400., dtype=np.float64)
+
+    # Coordinates of the test fieldset (on A-grid in deg)
+    lon = np.linspace(0, 4, xdim, dtype=np.float32)
+    lat = np.linspace(45, 52, ydim, dtype=np.float32)
+
+    # Grid spacing in m
+    def cosd(x):
+        return math.cos(math.radians(float(x)))
+    dx = (lon[1] - lon[0]) * 1852 * 60 * cosd(lat.mean())
+    dy = (lat[1] - lat[0]) * 1852 * 60
+
+    # Define arrays U (zonal), V (meridional), W (vertical) and P (sea
+    # surface height) all on A-grid
+    U = np.zeros((lon.size, lat.size, time.size), dtype=np.float32)
+    V = np.zeros((lon.size, lat.size, time.size), dtype=np.float32)
+    P = np.zeros((lon.size, lat.size, time.size), dtype=np.float32)
+
+    # Some constants
+    corio_0 = 1.e-4  # Coriolis parameter
+    h0 = 1  # Max eddy height
+    sig = 0.5  # Eddy e-folding decay scale (in degrees)
+    g = 10  # Gravitational constant
+    eddyspeed = 0.1  # Translational speed in m/s
+    dX = eddyspeed * 86400 / dx  # Grid cell movement of eddy max each day
+    dY = eddyspeed * 86400 / dy  # Grid cell movement of eddy max each day
+
+    [x, y] = np.mgrid[:lon.size, :lat.size]
+    for t in range(time.size):
+        hymax_1 = lat.size / 7.
+        hxmax_1 = .75 * lon.size - dX * t
+        hymax_2 = 3. * lat.size / 7. + dY * t
+        hxmax_2 = .75 * lon.size - dX * t
+
+        P[:, :, t] = h0 * np.exp(-(x-hxmax_1)**2/(sig*lon.size/4.)**2-(y-hymax_1)**2/(sig*lat.size/7.)**2)
+        P[:, :, t] += h0 * np.exp(-(x-hxmax_2)**2/(sig*lon.size/4.)**2-(y-hymax_2)**2/(sig*lat.size/7.)**2)
+
+        V[:-1, :, t] = -np.diff(P[:, :, t], axis=0) / dx / corio_0 * g
+        V[-1, :, t] = V[-2, :, t]  # Fill in the last column
+
+        U[:, :-1, t] = np.diff(P[:, :, t], axis=1) / dy / corio_0 * g
+        U[:, -1, t] = U[:, -2, t]  # Fill in the last row
+
+    data = {'U': U, 'V': V, 'P': P}
+    dimensions = {'lon': lon, 'lat': lat, 'depth': depth, 'time': time}
+    return FieldSet.from_data(data, dimensions)
+
+
+def moving_eddies_example(fieldset, npart=2, mode='jit', verbose=False,
+                          method=AdvectionRK4):
+    """Configuration of a particle set that follows two moving eddies
+
+    :arg fieldset: :class FieldSet: that defines the flow field
+    :arg npart: Number of particles to initialise"""
+
+    # Determine particle class according to mode
+    pset = ParticleSet.from_line(fieldset=fieldset, size=npart, pclass=ptype[mode],
+                                 start=(3.3, 46.), finish=(3.3, 47.8))
+
+    if verbose:
+        print("Initial particle positions:\n%s" % pset)
+
+    # Execute for 1 week, with 1 hour timesteps and hourly output
+    endtime = delta(days=7)
+    print("MovingEddies: Advecting %d particles for %s" % (npart, str(endtime)))
+    pset.execute(method, endtime=endtime, dt=delta(hours=1),
+                 output_file=pset.ParticleFile(name="EddyParticle"),
+                 interval=delta(hours=1), show_movie=False)
+
+    if verbose:
+        print("Final particle positions:\n%s" % pset)
+
+    return pset
+
+
+@pytest.mark.parametrize('mode', ['scipy', 'jit'])
+def test_moving_eddies_fwdbwd(mode, npart=2):
+    method = AdvectionRK4
+    fieldset = moving_eddies_fieldset()
+
+    # Determine particle class according to mode
+    pset = ParticleSet.from_line(fieldset=fieldset, size=npart, pclass=ptype[mode],
+                                 start=(3.3, 46.), finish=(3.3, 47.8))
+
+    # Execte for 14 days, with 30sec timesteps and hourly output
+    endtime = delta(days=1)
+    dt = delta(minutes=5)
+    interval = delta(hours=1)
+    print("MovingEddies: Advecting %d particles for %s" % (npart, str(endtime)))
+    pset.execute(method, endtime=endtime, dt=dt, interval=interval,
+                 output_file=pset.ParticleFile(name="EddyParticlefwd"))
+
+    print("Now running in backward time mode")
+    pset.execute(method, endtime=0, dt=-dt, interval=-interval,
+                 output_file=pset.ParticleFile(name="EddyParticlebwd"))
+
+    assert(pset[0].lon > 3.2 and 45.9 < pset[0].lat < 46.1)
+    assert(pset[1].lon > 3.2 and 47.7 < pset[1].lat < 47.9)
+
+    return pset
+
+
+@pytest.mark.parametrize('mode', ['scipy', 'jit'])
+def test_moving_eddies_fieldset(mode):
+    fieldset = moving_eddies_fieldset()
+    pset = moving_eddies_example(fieldset, 2, mode=mode)
+    assert(pset[0].lon < 2.0 and 46.2 < pset[0].lat < 46.25)
+    assert(pset[1].lon < 2.0 and 48.8 < pset[1].lat < 48.85)
+
+
+@pytest.fixture(scope='module')
+def fieldsetfile():
+    """Generate fieldset files for moving_eddies test"""
+    filename = 'moving_eddies'
+    fieldset = moving_eddies_fieldset(200, 350)
+    fieldset.write(filename)
+    return filename
+
+
+@pytest.mark.parametrize('mode', ['scipy', 'jit'])
+def test_moving_eddies_file(fieldsetfile, mode):
+    fieldset = FieldSet.from_nemo(fieldsetfile, extra_fields={'P': 'P'})
+    pset = moving_eddies_example(fieldset, 2, mode=mode)
+    assert(pset[0].lon < 2.0 and 46.2 < pset[0].lat < 46.25)
+    assert(pset[1].lon < 2.0 and 48.8 < pset[1].lat < 48.85)
+
+
+if __name__ == "__main__":
+    p = ArgumentParser(description="""
+Example of particle advection around an idealised peninsula""")
+    p.add_argument('mode', choices=('scipy', 'jit'), nargs='?', default='jit',
+                   help='Execution mode for performing RK4 computation')
+    p.add_argument('-p', '--particles', type=int, default=2,
+                   help='Number of particles to advect')
+    p.add_argument('-v', '--verbose', action='store_true', default=False,
+                   help='Print particle information before and after execution')
+    p.add_argument('--profiling', action='store_true', default=False,
+                   help='Print profiling information after run')
+    p.add_argument('-f', '--fieldset', type=int, nargs=2, default=None,
+                   help='Generate fieldset file with given dimensions')
+    p.add_argument('-m', '--method', choices=('RK4', 'EE', 'RK45'), default='RK4',
+                   help='Numerical method used for advection')
+    args = p.parse_args()
+    filename = path.join(path.dirname(__file__), 'MovingEddies_data', 'moving_eddies')
+
+    # Generate fieldset files according to given dimensions
+    if args.fieldset is not None:
+        fieldset = moving_eddies_fieldset(args.fieldset[0], args.fieldset[1])
+        fieldset.write(filename)
+
+    # Open fieldset files
+    fieldset = FieldSet.from_nemo(filename, extra_fields={'P': 'P'})
+
+    if args.profiling:
+        from cProfile import runctx
+        from pstats import Stats
+        runctx("moving_eddies_example(fieldset, args.particles, mode=args.mode, \
+                              verbose=args.verbose, method=method[args.method])",
+               globals(), locals(), "Profile.prof")
+        Stats("Profile.prof").strip_dirs().sort_stats("time").print_stats(10)
+    else:
+        moving_eddies_example(fieldset, args.particles, mode=args.mode,
+                              verbose=args.verbose, method=method[args.method])
---
+++ Miniproject/Parcels Learning/parcels_examples/example_peninsula.py
@@ -0,0 +1,200 @@
+from parcels import FieldSet, ParticleSet, ScipyParticle, JITParticle, Variable
+from parcels import AdvectionRK4, AdvectionEE, AdvectionRK45
+from argparse import ArgumentParser
+import numpy as np
+import math  # NOQA
+import pytest
+from datetime import timedelta as delta
+
+
+ptype = {'scipy': ScipyParticle, 'jit': JITParticle}
+method = {'RK4': AdvectionRK4, 'EE': AdvectionEE, 'RK45': AdvectionRK45}
+
+
+def peninsula_fieldset(xdim, ydim):
+    """Construct a fieldset encapsulating the flow field around an
+    idealised peninsula.
+
+    :param xdim: Horizontal dimension of the generated fieldset
+    :param xdim: Vertical dimension of the generated fieldset
+
+    The original test description can be found in Fig. 2.2.3 in:
+    North, E. W., Gallego, A., Petitgas, P. (Eds). 2009. Manual of
+    recommended practices for modelling physical - biological
+    interactions during fish early life.
+    ICES Cooperative Research Report No. 295. 111 pp.
+    http://archimer.ifremer.fr/doc/00157/26792/24888.pdf
+
+    Note that the problem is defined on an A-grid while NEMO
+    normally returns C-grids. However, to avoid accuracy
+    problems with interpolation from A-grid to C-grid, we
+    return NetCDF files that are on an A-grid.
+    """
+    # Set NEMO fieldset variables
+    depth = np.zeros(1, dtype=np.float32)
+    time = np.zeros(1, dtype=np.float64)
+
+    # Generate the original test setup on A-grid in km
+    dx = 100. / xdim / 2.
+    dy = 50. / ydim / 2.
+    La = np.linspace(dx, 100.-dx, xdim, dtype=np.float32)
+    Wa = np.linspace(dy, 50.-dy, ydim, dtype=np.float32)
+
+    # Define arrays U (zonal), V (meridional), W (vertical) and P (sea
+    # surface height) all on A-grid
+    U = np.zeros((xdim, ydim), dtype=np.float32)
+    V = np.zeros((xdim, ydim), dtype=np.float32)
+    W = np.zeros((xdim, ydim), dtype=np.float32)
+    P = np.zeros((xdim, ydim), dtype=np.float32)
+
+    u0 = 1
+    x0 = 50.
+    R = 0.32 * 50.
+
+    # Create the fields
+    x, y = np.meshgrid(La, Wa, sparse=True, indexing='ij')
+    P = u0*R**2*y/((x-x0)**2+y**2)-u0*y
+    U = u0-u0*R**2*((x-x0)**2-y**2)/(((x-x0)**2+y**2)**2)
+    V = -2*u0*R**2*((x-x0)*y)/(((x-x0)**2+y**2)**2)
+
+    # Set land points to NaN
+    landpoints = P >= 0.
+    U[landpoints] = np.nan
+    V[landpoints] = np.nan
+    W[landpoints] = np.nan
+
+    # Convert from km to lat/lon
+    lon = La / 1.852 / 60.
+    lat = Wa / 1.852 / 60.
+
+    data = {'U': U, 'V': V, 'P': P}
+    dimensions = {'lon': lon, 'lat': lat, 'depth': depth, 'time': time}
+    return FieldSet.from_data(data, dimensions)
+
+
+def UpdateP(particle, fieldset, time, dt):
+    particle.p = fieldset.P[time, particle.lon, particle.lat, particle.depth]
+
+
+def pensinsula_example(fieldset, npart, mode='jit', degree=1,
+                       verbose=False, output=True, method=AdvectionRK4):
+    """Example configuration of particle flow around an idealised Peninsula
+
+    :arg filename: Basename of the input fieldset
+    :arg npart: Number of particles to intialise"""
+
+    # First, we define a custom Particle class to which we add a
+    # custom variable, the initial stream function value p.
+    # We determine the particle base class according to mode.
+    class MyParticle(ptype[mode]):
+        # JIT compilation requires a-priori knowledge of the particle
+        # data structure, so we define additional variables here.
+        p = Variable('p', dtype=np.float32, initial=0.)
+        p_start = Variable('p_start', dtype=np.float32, initial=fieldset.P)
+
+        def __repr__(self):
+            """Custom print function which overrides the built-in"""
+            return "P(%.4f, %.4f)[p=%.5f, p_start=%f]" % (self.lon, self.lat,
+                                                          self.p, self.p_start)
+
+    # Initialise particles
+    x = 3. * (1. / 1.852 / 60)  # 3 km offset from boundary
+    y = (fieldset.U.lat[0] + x, fieldset.U.lat[-1] - x)  # latitude range, including offsets
+    pset = ParticleSet.from_line(fieldset, size=npart, pclass=MyParticle,
+                                 start=(x, y[0]), finish=(x, y[1]), time=0)
+
+    if verbose:
+        print("Initial particle positions:\n%s" % pset)
+
+    # Advect the particles for 24h
+    time = delta(hours=24)
+    dt = delta(minutes=5)
+    k_adv = pset.Kernel(method)
+    k_p = pset.Kernel(UpdateP)
+    out = pset.ParticleFile(name="MyParticle") if output else None
+    interval = delta(hours=1) if output else -1
+    print("Peninsula: Advecting %d particles for %s" % (npart, str(time)))
+    pset.execute(k_adv + k_p, endtime=time, dt=dt, output_file=out, interval=interval)
+
+    if verbose:
+        print("Final particle positions:\n%s" % pset)
+
+    return pset
+
+
+@pytest.mark.parametrize('mode', ['scipy', 'jit'])
+def test_peninsula_fieldset(mode):
+    """Execute peninsula test from fieldset generated in memory"""
+    fieldset = peninsula_fieldset(100, 50)
+    pset = pensinsula_example(fieldset, 5, mode=mode, degree=1)
+    # Test advection accuracy by comparing streamline values
+    err_adv = np.array([abs(p.p_start - p.p) for p in pset])
+    assert(err_adv <= 1.e-3).all()
+    # Test Field sampling accuracy by comparing kernel against Field sampling
+    err_smpl = np.array([abs(p.p - pset.fieldset.P[0., p.lon, p.lat, p.depth]) for p in pset])
+    assert(err_smpl <= 1.e-3).all()
+
+
+@pytest.fixture(scope='module')
+def fieldsetfile():
+    """Generate fieldset files for peninsula test"""
+    filename = 'peninsula'
+    fieldset = peninsula_fieldset(100, 50)
+    fieldset.write(filename)
+    return filename
+
+
+@pytest.mark.parametrize('mode', ['scipy', 'jit'])
+def test_peninsula_file(fieldsetfile, mode):
+    """Open fieldset files and execute"""
+    fieldset = FieldSet.from_nemo(fieldsetfile, extra_fields={'P': 'P'}, allow_time_extrapolation=True)
+    pset = pensinsula_example(fieldset, 5, mode=mode, degree=1)
+    # Test advection accuracy by comparing streamline values
+    err_adv = np.array([abs(p.p_start - p.p) for p in pset])
+    assert(err_adv <= 1.e-3).all()
+    # Test Field sampling accuracy by comparing kernel against Field sampling
+    err_smpl = np.array([abs(p.p - pset.fieldset.P[0., p.lon, p.lat, p.depth]) for p in pset])
+    assert(err_smpl <= 1.e-3).all()
+
+
+if __name__ == "__main__":
+    p = ArgumentParser(description="""
+Example of particle advection around an idealised peninsula""")
+    p.add_argument('mode', choices=('scipy', 'jit'), nargs='?', default='jit',
+                   help='Execution mode for performing RK4 computation')
+    p.add_argument('-p', '--particles', type=int, default=20,
+                   help='Number of particles to advect')
+    p.add_argument('-d', '--degree', type=int, default=1,
+                   help='Degree of spatial interpolation')
+    p.add_argument('-v', '--verbose', action='store_true', default=False,
+                   help='Print particle information before and after execution')
+    p.add_argument('-o', '--nooutput', action='store_true', default=False,
+                   help='Suppress trajectory output')
+    p.add_argument('--profiling', action='store_true', default=False,
+                   help='Print profiling information after run')
+    p.add_argument('-f', '--fieldset', type=int, nargs=2, default=None,
+                   help='Generate fieldset file with given dimensions')
+    p.add_argument('-m', '--method', choices=('RK4', 'EE', 'RK45'), default='RK4',
+                   help='Numerical method used for advection')
+    args = p.parse_args()
+
+    if args.fieldset is not None:
+        filename = 'peninsula'
+        fieldset = peninsula_fieldset(args.fieldset[0], args.fieldset[1])
+        fieldset.write(filename)
+
+    # Open fieldset file set
+    fieldset = FieldSet.from_nemo('peninsula', extra_fields={'P': 'P'}, allow_time_extrapolation=True)
+
+    if args.profiling:
+        from cProfile import runctx
+        from pstats import Stats
+        runctx("pensinsula_example(fieldset, args.particles, mode=args.mode,\
+                                   degree=args.degree, verbose=args.verbose,\
+                                   output=not args.nooutput, method=method[args.method])",
+               globals(), locals(), "Profile.prof")
+        Stats("Profile.prof").strip_dirs().sort_stats("time").print_stats(10)
+    else:
+        pensinsula_example(fieldset, args.particles, mode=args.mode,
+                           degree=args.degree, verbose=args.verbose,
+                           output=not args.nooutput, method=method[args.method])
