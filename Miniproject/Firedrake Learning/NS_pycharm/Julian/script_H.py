d1b3e79e760d6e03b0bcd0310abe42fa367d4570
Alexander Kier Christensen <akc17@ic.ac.uk>
2018-02-16 15:20:03 +0000

    1) wrote offline_Hgrid_generator.py to produce cartesian flow field given a single timestep solution for H
    2) added folder to store offline fields to Parcels Learning directory

--- Miniproject/Firedrake Learning/NS_pycharm/Julian/script_H.py
+++ Miniproject/Firedrake Learning/NS_pycharm/Julian/script_H.py
@@ -1,12 +1,13 @@
+import os, shutil
 from incflow import *
 from incflow.inc_navier_stokes import IncNavierStokes
 from firedrake import *
 from tqdm import tqdm
 
-mesh = Mesh("/home/alexander/Documents/QMEE/Miniproject/Firedrake Learning/meshes/H_low.msh")
+mesh = Mesh("/home/alexander/Documents/QMEE/Miniproject/Firedrake Learning/meshes/H.msh")
 rho = 1
 nu = 0.01
-dt = 0.05
+dt = 0.01
 
 INS = IncNavierStokes(mesh, nu, rho, dt)
 W = INS.get_mixed_fs()
@@ -18,20 +19,30 @@ INS.setup_solver()
 
 step = 0
 t = 0.0
-t_end = 600
+t_end = 30
 num_steps = int((t_end - t)/INS.dt)
 
-folderstr = str(t_end) + "T_" + str(dt) + "dt_" + str(rho*nu) + "mu"
-outfile_u = File("/home/alexander/Documents/QMEE/Miniproject/Firedrake Learning/NS_tutorial_saves/NS_H/Julian/" + folderstr + "/u.pvd")
-outfile_p = File("/home/alexander/Documents/QMEE/Miniproject/Firedrake Learning/NS_tutorial_saves/NS_H/Julian/" + folderstr + "/p.pvd")
+simstr = str(t_end) + "T_" + str(dt) + "dt_" + str(rho*nu) + "mu"
+folderstr = "/home/alexander/Documents/QMEE/Miniproject/Firedrake Learning/NS_tutorial_saves/NS_H/Julian/" + simstr
+# if os.path.isdir(folderstr):  # Empty directory if already existing.
+#     shutil.rmtree(folderstr)
+#     os.mkdir(folderstr)
+outfile_u = File(folderstr + "/u.pvd")
+outfile_p = File(folderstr + "/p.pvd")
 
 for steps in tqdm(range(num_steps)):
     t += INS.dt
 
     u_sol, p_sol = INS.step()
 
-    if steps % 50 == 0:
-        outfile_u.write(u_sol)
-        outfile_p.write(p_sol)
+#    if steps % 10 == 0:
+    outfile_u.write(u_sol)
+    outfile_p.write(p_sol)
 
+
+# Checkpoint final state.
+chk_out = checkpointing.HDF5File(folderstr + "/final.h5", file_mode='w')
+chk_out.write(u_sol, "/velocity")
+chk_out.write(p_sol, "/pressure")
+chk_out.close()
 
---
+++ Miniproject/Firedrake Learning/NS_pycharm/offline_Hgrid_generator.py
@@ -0,0 +1,22 @@
+from firedrake import *
+import numpy as np
+
+mesh = Mesh("/home/alexander/Documents/QMEE/Miniproject/Firedrake Learning/meshes/H_low.msh")
+V = VectorFunctionSpace(mesh, "CG", 2)
+
+u = Function(V)
+chk_in = checkpointing.HDF5File(
+    "/home/alexander/Documents/QMEE/Miniproject/Firedrake Learning/NS_tutorial_saves/NS_H/Julian/60T_0.05dt_0.01mu/final.h5",
+    file_mode='r')
+chk_in.read(u, "/velocity")
+chk_in.close()
+
+# Generate cartesian velocity field
+u_field_1 = np.array([[u.at(x, y) for x in np.arange(0, 2.8, 13.6/100)] for y in np.arange(13.6 - 13.6/100, 0, -13.6/100)])
+u_field_2 = np.vstack(([[(0, 0) for x in np.arange(2.8 + 13.6/100, 10.8, 13.6/100)] for y in np.arange(13.6 - 13.6/100, 8.8, -13.6/100)],
+                      [[u.at([x, y]) for x in np.arange(2.8 + 13.6/100, 10.8, 13.6/100)] for y in np.arange(8.8 - 13.6/100, 4.8, -13.6/100)],
+                      [[(0, 0) for x in np.arange(2.8 + 13.6/100, 10.8, 13.6/100)] for y in np.arange(4.8 - 13.6/100, 0, -13.6/100)]))
+u_field_3 = np.array([[u.at(x, y) for x in np.arange(10.8 + 13.6/100, 13.6, 13.6/100)] for y in np.arange(13.6 - 13.6/100, 0, -13.6/100)])
+u_field = np.hstack((u_field_1, u_field_2, u_field_3))
+
+np.save("/home/alexander/Documents/QMEE/Miniproject/Parcels Learning/fields/offline_grid_H_low.npy", u_field)
